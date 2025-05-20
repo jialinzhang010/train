c9f32689c717eff0602bc3a9c515244ef4ef1b6a
Jialin <zhang030116@126.com>
2025-05-20 00:27:03 -0700

    modified startup log, added /member. modified logback log

--- member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
+++ member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
@@ -1,14 +1,21 @@
 package com.jiawa.train.member.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.jiawa")
 public class MemberApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("Successful startup!");
+        LOG.info("Testing Address: \thttp://127.0.0.1:{}/hello", env.getProperty("server.port"));
     }
 
 }
