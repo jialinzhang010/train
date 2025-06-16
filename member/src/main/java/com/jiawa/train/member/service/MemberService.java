5b2e5626b977ee9880da5c35cfbc6f2d7a527b2a
Jialin <zhang030116@126.com>
2025-06-15 21:54:39 -0700

    Encapsulate request parameters and returned results

--- member/src/main/java/com/jiawa/train/member/service/MemberService.java
+++ member/src/main/java/com/jiawa/train/member/service/MemberService.java
@@ -4,6 +4,7 @@ import cn.hutool.core.collection.CollUtil;
 import com.jiawa.train.member.domain.Member;
 import com.jiawa.train.member.domain.MemberExample;
 import com.jiawa.train.member.mapper.MemberMapper;
+import com.jiawa.train.member.req.MemberRegisterReq;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +17,8 @@ public class MemberService {
     public int count() {
         return Math.toIntExact(memberMapper.countByExample(null));
     }
-    public long register(String mobile) {
+    public long register(MemberRegisterReq req) {
+        String mobile = req.getMobile();
         MemberExample memberExample = new MemberExample();
         memberExample.createCriteria().andMobileEqualTo(mobile);
         List<Member> list = memberMapper.selectByExample(memberExample);
