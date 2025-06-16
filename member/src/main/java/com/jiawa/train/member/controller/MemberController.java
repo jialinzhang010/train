0e5bc1cff566de58591d2567f8a5d7672552eaaf
Jialin <zhang030116@126.com>
2025-06-15 22:11:02 -0700

    add common exception handler

--- member/src/main/java/com/jiawa/train/member/controller/MemberController.java
+++ member/src/main/java/com/jiawa/train/member/controller/MemberController.java
@@ -29,9 +29,5 @@ public class MemberController {
 //        return commonResp;
         return new CommonResp<>(register);
     }
-    @GetMapping("/test")
-    public CommonResp test() {
-        throw new RuntimeException("手动测试异常");
-    }
 }
 
