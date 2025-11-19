316ae4b1478d6596fd5ba640d3529d880d53ed39
jialinzhang010 <jialinzhang1209@outlook.com>
2025-11-19 15:56:34 -0800

    4.4 Modify launch log, add /member, modify logback log

--- member/src/main/java/com/jialin/train/member/config/MemberApplication.java
+++ member/src/main/java/com/jialin/train/member/config/MemberApplication.java
@@ -1,15 +1,22 @@
 package com.jialin.train.member.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.jialin")
 public class MemberApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
-    }
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
 
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("Launched successfully!");
+        LOG.info("URL: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+    }
 }
