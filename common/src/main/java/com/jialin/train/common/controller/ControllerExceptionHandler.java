5d46b4f082e775d89a6009c0e317d6da538b704e
jialinzhang010 <jialinzhang1209@outlook.com>
2025-11-22 15:19:35 -0800

    4.17 Add Unified Exception Handling

---
+++ common/src/main/java/com/jialin/train/common/controller/ControllerExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.jialin.train.common.controller;
+
+import com.jialin.train.common.resp.CommonResp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+    private static final Logger LOG = LoggerFactory.getLogger(ControllerExceptionHandler.class);
+    /**
+     * All exception handler
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public CommonResp exceptionHandler(Exception e) {
+        CommonResp commonResp = new CommonResp();
+        LOG.error("Exception: ", e);
+        commonResp.setSuccess(false);
+//        commonResp.setMessage("Exception, please contact admin");
+        commonResp.setMessage(e.getMessage());
+        return commonResp;
+
+    }
+
+}
