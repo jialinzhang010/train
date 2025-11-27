26eaa8a6b8a88d8c801e9c3f344bd6821238a017
jialinzhang010 <jialinzhang1209@outlook.com>
2025-11-26 19:01:26 -0800

    6.10 Add router interceptor

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from "@/store";
+import {notification} from "ant-design-vue";
 
 const routes = [
   {
@@ -7,13 +9,35 @@ const routes = [
   },
   {
     path: '/',
-    component: () => import('../views/main.vue')
+    component: () => import('../views/main.vue'),
+    meta: {
+      requireLogin: true
+    }
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(function(item) {
+    console.log(item, "Require login verification or not: ", item.meta.requireLogin || false);
+    return item.meta.requireLogin;
+  })) {
+    const _member = store.state.member;
+    console.log("Start login verification: ", _member);
+    if (!_member.token) {
+      console.log("User did not log in or session expired!");
+      notification.error({ description: "Not logged in or session expired" });
+      next("/login");
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 })
 
 export default router
