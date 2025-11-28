7c1e655e2de38febc0429fd76093e927a7062369
jialinzhang010 <jialinzhang1209@outlook.com>
2025-11-27 16:45:58 -0800

    7.2 Add passenger table

--- sql/member.sql
+++ sql/member.sql
@@ -5,3 +5,16 @@ create table `member` (
     primary key (`id`),
     unique key `mobile_unique` (`mobile`)
 ) engine=innodb default charset=utf8mb4 comment='member';
+
+drop table if exists `passenger`;
+create table `passenger` (
+    `id` bigint not null comment 'id',
+    `member_id` bigint not null comment 'member id',
+    `name` varchar(20) not null comment 'name',
+    `id_card` varchar(18) not null comment 'ID card',
+    `type` char(1) not null comment 'passenger type|enum[PassengerTypeEnum]',
+    `create_time` datetime(3) comment 'create time',
+    `update_time` datetime(3) comment 'update time',
+    primary key (`id`),
+    index `member_id_index` (`member_id`)
+) engine=innodb default charset=utf8mb4 comment='passenger';
