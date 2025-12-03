f2a64b34d84d8e64ee8071a856bc6870a9537460
jialinzhang010 <jialinzhang1209@outlook.com>
2025-12-03 15:39:35 -0800

    8.4 Integrate dom4j, read pom.xml, get current persistence layer file

---
+++ generator/src/main/java/com/jialin/train/generator/server/ServerGenerator.java
@@ -0,0 +1,27 @@
+package com.jialin.train.generator.server;
+
+import org.dom4j.Document;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ServerGenerator {
+    static String toPath = "generator/src/main/java/com/jialin/train/generator/";
+    static String pomPath = "generator/pom.xml";
+    static {
+        new File(toPath).mkdirs();
+    }
+
+    public static void main(String[] args) throws Exception {
+        SAXReader saxReader = new SAXReader();
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("pom", "http://maven.apache.org/POM/4.0.0");
+        saxReader.getDocumentFactory().setXPathNamespaceURIs(map);
+        Document document = saxReader.read(pomPath);
+        Node node = document.selectSingleNode("//pom:configurationFile");
+        System.out.println(node.getText());
+    }
+}
