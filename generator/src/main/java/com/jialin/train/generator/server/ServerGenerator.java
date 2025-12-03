3c13424608fa207bd77e800c0b71174b21efce37
jialinzhang010 <jialinzhang1209@outlook.com>
2025-12-03 15:48:54 -0800

    8.4 Integrate dom4j, read pom.xml, read current persistence layer file, get table name

--- generator/src/main/java/com/jialin/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/jialin/train/generator/server/ServerGenerator.java
@@ -1,6 +1,7 @@
 package com.jialin.train.generator.server;
 
 import org.dom4j.Document;
+import org.dom4j.DocumentException;
 import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
 
@@ -16,6 +17,17 @@ public class ServerGenerator {
     }
 
     public static void main(String[] args) throws Exception {
+        String generatorPath = getGeneratorPath();
+
+        Document document = new SAXReader().read("generator/" + generatorPath);
+        Node table = document.selectSingleNode("//table");
+        System.out.println(table);
+        Node tableName = table.selectSingleNode("@tableName");
+        Node domainObjectName = table.selectSingleNode("@domainObjectName");
+        System.out.println(tableName.getText() + "/" + domainObjectName.getText());
+    }
+
+    private static String getGeneratorPath() throws DocumentException {
         SAXReader saxReader = new SAXReader();
         Map<String, String> map = new HashMap<String, String>();
         map.put("pom", "http://maven.apache.org/POM/4.0.0");
@@ -23,5 +35,6 @@ public class ServerGenerator {
         Document document = saxReader.read(pomPath);
         Node node = document.selectSingleNode("//pom:configurationFile");
         System.out.println(node.getText());
+        return node.getText();
     }
 }
