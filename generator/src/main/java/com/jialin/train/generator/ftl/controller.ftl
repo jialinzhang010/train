e4677e8b7b664467c13ca63f7be628c8b44fc07b
jialinzhang010 <jialinzhang1209@outlook.com>
2025-12-03 16:42:55 -0800

    8.5 Use PassengerController to make template

---
+++ generator/src/main/java/com/jialin/train/generator/ftl/controller.ftl
@@ -0,0 +1,39 @@
+package com.jialin.train.member.controller;
+
+import com.jialin.train.common.context.LoginMemberContext;
+import com.jialin.train.common.resp.CommonResp;
+import com.jialin.train.common.resp.PageResp;
+import com.jialin.train.member.req.${Domain}QueryReq;
+import com.jialin.train.member.req.${Domain}SaveReq;
+import com.jialin.train.member.resp.${Domain}QueryResp;
+import com.jialin.train.member.service.${Domain}Service;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/${domain}")
+public class ${Domain}Controller {
+    @Resource
+    private ${Domain}Service ${domain}Service;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody ${Domain}SaveReq req) {
+        ${domain}Service.save(req);
+        return new CommonResp<>();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<${Domain}QueryResp>> queryList(@Valid ${Domain}QueryReq req) {
+        req.setMemberId(LoginMemberContext.getId());
+        PageResp<${Domain}QueryResp> list = ${domain}Service.queryList(req);
+        return new CommonResp<>(list);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp<Object> delete(@PathVariable("id") Long id) {
+        ${domain}Service.delete(id);
+        return new CommonResp<>();
+    }
+
+}
--- generator/src/main/java/com/jialin/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/jialin/train/generator/server/ServerGenerator.java
@@ -1,20 +1,22 @@
 package com.jialin.train.generator.server;
 
 import com.jialin.train.generator.util.FreemarkerUtil;
+import freemarker.template.TemplateException;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ServerGenerator {
-    static String servicePath = "[module]/src/main/java/com/jialin/train/[module]/service/";
+    static String serverPath = "[module]/src/main/java/com/jialin/train/[module]/";
     static String pomPath = "generator/pom.xml";
     static {
-        new File(servicePath).mkdirs();
+        new File(serverPath).mkdirs();
     }
 
     public static void main(String[] args) throws Exception {
@@ -23,8 +25,8 @@ public class ServerGenerator {
 
         String module = generatorPath.replace("src/main/resources/generator-config-", "").replace(".xml", "");
         System.out.println("module: " + module);
-        servicePath = servicePath.replace("[module]", module);
-        System.out.println("servicePath: " + servicePath);
+        serverPath = serverPath.replace("[module]", module);
+        System.out.println("servicePath: " + serverPath);
 
         Document document = new SAXReader().read("generator/" + generatorPath);
         Node table = document.selectSingleNode("//table");
@@ -45,8 +47,18 @@ public class ServerGenerator {
         param.put("module", module);
         System.out.println("param: " + param);
 
-        FreemarkerUtil.initConfig("service.ftl");
-        FreemarkerUtil.generator(servicePath + Domain + "Service.java", param);
+        gen(Domain, param, "service");
+        gen(domain, param, "controller");
+    }
+
+    private static void gen(String Domain, Map<String, Object> param, String target) throws IOException, TemplateException {
+        FreemarkerUtil.initConfig(target + ".ftl");
+        String toPath = serverPath + target + "/";
+        new File(toPath).mkdirs();
+        String Target = target.substring(0, 1).toUpperCase() + target.substring(1);
+        String fileName = toPath + Domain + Target + ".java";
+        System.out.println("Generating: " + fileName);
+        FreemarkerUtil.generator(fileName, param);
     }
 
     private static String getGeneratorPath() throws DocumentException {
