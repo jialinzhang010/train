9a14728a7655891cebfd365c3a6a6b3134765d6b
jialinzhang010 <jialinzhang1209@outlook.com>
2025-12-06 15:51:32 -0800

    9.4 Add business module

---
+++ business/src/main/java/com/jialin/train/business/config/BusinessApplication.java
@@ -0,0 +1,24 @@
+package com.jialin.train.business.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@ComponentScan("com.jialin")
+@MapperScan("com.jialin.train.*.mapper")
+public class BusinessApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BusinessApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(BusinessApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("Launched successfully!");
+        LOG.info("URL: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+    }
+}
