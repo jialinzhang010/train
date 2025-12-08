b89300281d94aa8500eaf089464d304e31c0ce97
jialinzhang010 <jialinzhang1209@outlook.com>
2025-12-08 14:46:21 -0800

    9.12 Add Train code dropdown backend api, frontend adds train code dropdown

--- admin/src/views/main/train-station.vue
+++ admin/src/views/main/train-station.vue
@@ -28,7 +28,12 @@
            ok-text="Confirm" cancel-text="Cancel">
     <a-form :model="trainStation" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
       <a-form-item label="Train code">
-        <a-input v-model:value="trainStation.trainCode" />
+        <a-select v-model:value="trainStation.trainCode" show-search
+                  :filterOption="filterTrainCodeOption">
+          <a-select-option v-for="item in trains" :key="item.code" :value="item.code" :label="item.code + item.start + item.end" >
+            {{item.code}} | {{item.start}} ~ {{item.end}}
+          </a-select-option>
+        </a-select>
       </a-form-item>
       <a-form-item label="Station order">
         <a-input v-model:value="trainStation.index" />
@@ -197,15 +202,22 @@ export default defineComponent({
       });
     };
 
+    const trains = ref([]);
+
     const queryTrainCode = () => {
       axios.get("/business/admin/train/query-all").then((response) => {
         let data = response.data;
         if (data.success) {
-          console.log(data.content);
+          trains.value = data.content;
         } else {
           notification.error({description: data.message});
         }
       })
+    }
+
+    const filterTrainCodeOption = (input, option) => {
+      console.log(input, option);
+      return option.label.toLowerCase().indexOf((input.toLowerCase())) >= 0;
     }
 
     onMounted(() => {
@@ -228,7 +240,10 @@ export default defineComponent({
       onAdd,
       handleOk,
       onEdit,
-      onDelete
+      onDelete,
+      filterTrainCodeOption,
+      queryTrainCode,
+      trains
     };
   },
 });
