d752382a8a4d9a41086e0fd4151291bde84bff53
jialinzhang010 <jialinzhang1209@outlook.com>
2025-12-15 10:43:03 -0800

    10.2 Add batch, scheduled task module

---
+++ batch/src/main/java/com/jialin/train/batch/config/BatchApplication.java
@@ -0,0 +1,23 @@
+package com.jialin.train.batch.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@ComponentScan("com.jialin")
+//@MapperScan("com.jialin.train.*.mapper")
+public class BatchApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BatchApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(BatchApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("Launched successfully!");
+        LOG.info("URL: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+    }
+}
