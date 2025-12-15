a816d729270771fe4632883ff8197a746bdde913
jialinzhang010 <jialinzhang1209@outlook.com>
2025-12-15 13:35:52 -0800

    10.4 integrate quartz and springboot builtin scheduled tasks

--- batch/src/main/java/com/jialin/train/batch/config/BatchApplication.java
+++ batch/src/main/java/com/jialin/train/batch/config/BatchApplication.java
@@ -1,5 +1,6 @@
 package com.jialin.train.batch.config;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -9,7 +10,7 @@ import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.jialin")
-//@MapperScan("com.jialin.train.*.mapper")
+@MapperScan("com.jialin.train.*.mapper")
 public class BatchApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(BatchApplication.class);
---
+++ batch/src/main/java/com/jialin/train/batch/job/SpringBootTestJob.java
@@ -0,0 +1,19 @@
+package com.jialin.train.batch.job;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/*
+ * 适合单体应用，不适合集群
+ */
+@Component
+@EnableScheduling
+public class SpringBootTestJob {
+
+    @Scheduled(cron = "0/5 * * * * ?")
+    private void test() {
+        // 可以通过增加分布式锁解决集群问题
+        System.out.println("SpringBootTestJob test");
+    }
+}
