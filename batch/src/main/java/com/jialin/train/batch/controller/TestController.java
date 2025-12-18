da7eca514db827ff607080f405d2f5b3d56464ec
jialinzhang010 <jialinzhang1209@outlook.com>
2025-12-17 17:33:02 -0800

    11.8 integrate openfeign

--- batch/src/main/java/com/jialin/train/batch/controller/TestController.java
+++ batch/src/main/java/com/jialin/train/batch/controller/TestController.java
@@ -1,12 +1,24 @@
 package com.jialin.train.batch.controller;
 
+import com.jialin.train.batch.feign.BusinessFeign;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Resource
+    BusinessFeign businessFeign;
+
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+
     @GetMapping("/hello")
     public String hello() {
+        String businessHello = businessFeign.hello();
+        LOG.info(businessHello);
         return "hello world! Batch!";
     }
 }
