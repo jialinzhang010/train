bca3527987371b275ca6f6d66299c186c7e274fd
jialinzhang010 <jialinzhang1209@outlook.com>
2025-12-17 17:00:20 -0800

    11.7 Add scheduled task to generate daily train data

---
+++ batch/src/main/java/com/jialin/train/batch/job/DailyTrainJob.java
@@ -0,0 +1,23 @@
+package com.jialin.train.batch.job;
+
+import cn.hutool.core.util.RandomUtil;
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+@DisallowConcurrentExecution
+public class DailyTrainJob implements Job {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DailyTrainJob.class);
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        MDC.put("LOG_ID", System.currentTimeMillis() + RandomUtil.randomString(3));
+        LOG.info("Start generating daily train data.");
+        LOG.info("Finished generating daily train data.");
+
+    }
+}
