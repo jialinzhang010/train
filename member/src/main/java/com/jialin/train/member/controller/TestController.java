3512ee8b7e57cf1ad4a319887af739f5bfba41eb
jialinzhang010 <jialinzhang1209@outlook.com>
2025-12-27 17:41:57 -0800

    13.4 Integrate nacos

--- member/src/main/java/com/jialin/train/member/controller/TestController.java
+++ member/src/main/java/com/jialin/train/member/controller/TestController.java
@@ -1,12 +1,21 @@
 package com.jialin.train.member.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class TestController {
+
+    @Value("${test.nacos}")
+    private String testNacos;
+
     @GetMapping("/hello")
     public String hello() {
-        return "hello world123";
+        return String.format("Hello %s!", testNacos);
     }
+
+
 }
